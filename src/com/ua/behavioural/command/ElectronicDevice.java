4e3f40c088968c2c4b4e7251ff99db3eb9c24be4
YuriiAndreitsev <com.app.main@gmail.com>
2021-09-29 14:43:18 +0300

    studying command pattern

---
+++ src/com/ua/behavioural/command/ElectronicDevice.java
@@ -0,0 +1,13 @@
+package com.ua.behavioural.command;
+
+public interface ElectronicDevice {
+
+    public void on();
+
+    public void off();
+
+    public void volumeUp();
+
+    public void volumeDown();
+
+}
